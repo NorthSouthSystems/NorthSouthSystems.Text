dc8d907583b59e3d6a9a309db544eada956e1e14
SoftwareBotanyDan_cp <SND\SoftwareBotanyDan_cp>
2011-07-06 04:35:08 +0000

    Moving shared assembly attributes (such as AssemblyVersion) to sln level in preparation for first release.

---
+++ Main/AssemblyInfoShared.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SoftwareBotany.Ivy")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("Software Botany LLC.")]
+[assembly: AssemblyProduct("SoftwareBotany.Ivy")]
+[assembly: AssemblyCopyright("Copyright © Software Botany LLC. 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyVersion("0.9.0.0")]
