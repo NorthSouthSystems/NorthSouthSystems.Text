c54048be565af83ec37c261c25fdec84e225c760
FreeAndWithBeer <30539567+FreeAndWithBeer@users.noreply.github.com>
2023-07-25 22:05:36 -0400

    Added ToStringNullToEmpty extension method to allow method chaining to continue in the presense of a null object.

--- FOSStrich.Text.Test/String/Primitive/Extensions/Coalesce.cs
+++ FOSStrich.Text.Test/String/Primitive/Extensions/Coalesce.cs
@@ -19,5 +19,15 @@ public static partial class StringExtensionsTests
             foreach (string s in new string[] { " ", "a", "A", "1", "abc", "ABC", "123" })
                 Assert.AreEqual(s, s.EmptyToNull());
         }
+
+        [TestMethod]
+        public void ToStringNullToEmpty()
+        {
+            object obj = null;
+            Assert.AreEqual(string.Empty, obj.ToStringNullToEmpty());
+
+            int i = 1;
+            Assert.AreEqual("1", i.ToStringNullToEmpty());
+        }
     }
 }
--- FOSStrich.Text/String/Primitive/Extensions/Coalesce.cs
+++ FOSStrich.Text/String/Primitive/Extensions/Coalesce.cs
@@ -15,4 +15,18 @@ public static partial class StringExtensions
     /// </summary>
     /// <returns>Returns string.Empty when value == null; else, returns the original string.</returns>
     public static string NullToEmpty(this string value) => value ?? string.Empty;
+
+    /// <summary>
+    /// VIOLATES NULL REFERENCE SEMANTICS! Will return string.Empty if object == null. This method is provided so that
+    /// callers can use a fluent syntax for manipulation of the result of object.ToString(), which is not possible with the
+    /// C# 6 ?. operator which terminates the fluent method chain upon encountering null.
+    /// </summary>
+    /// <example>
+    /// object instance = GetInstance();
+    /// 
+    /// string s = instance.ToStringNullToEmpty()
+    ///     .Trim()
+    ///     .ToUpperInvariant();
+    /// </example>
+    public static string ToStringNullToEmpty(this object instance) => instance?.ToString() ?? string.Empty;
 }
