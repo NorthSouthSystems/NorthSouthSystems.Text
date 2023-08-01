013e9c5c25040c495e0cd5ff9d7f17f97a856d32
FreeAndWithBeer <30539567+FreeAndWithBeer@users.noreply.github.com>
2023-08-01 19:04:41 -0400

    Added Overwrite StringExtensions.

---
+++ FOSStrich.Text.Test/String/Primitive/Extensions/Overwrite.cs
@@ -0,0 +1,53 @@
+﻿namespace FOSStrich.Text;
+
+public static partial class StringExtensionsTests
+{
+    [TestClass]
+    public class Overwrite
+    {
+        [TestMethod]
+        public void Basic()
+        {
+            string result;
+
+            result = "abc".Overwrite(0, "z");
+            Assert.AreEqual("zbc", result);
+
+            result = "abc".Overwrite(1, "z");
+            Assert.AreEqual("azc", result);
+
+            result = "abc".Overwrite(1, "zy");
+            Assert.AreEqual("azy", result);
+
+            result = "abc".Overwrite(2, "zy");
+            Assert.AreEqual("abzy", result);
+
+            result = "abc".Overwrite(3, "zy");
+            Assert.AreEqual("abczy", result);
+        }
+
+        #region Exceptions
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ThisNull()
+        {
+            string s = null;
+            s.Overwrite(0, "z");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void StartIndexLessThanZero() => "a".Overwrite(-1, "z");
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void StartIndexGreaterThanValueLength() => "a".Overwrite(2, "z");
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NewValueNull() => "a".Overwrite(0, null);
+
+        #endregion
+    }
+}
---
+++ FOSStrich.Text/String/Primitive/Extensions/Overwrite.cs
@@ -0,0 +1,49 @@
+﻿namespace FOSStrich.Text;
+
+public static partial class StringExtensions
+{
+    /// <summary>
+    /// Overwrites characters in a string starting at a provided index.
+    /// </summary>
+    /// <param name="value">The string within which to overwrite characters.</param>
+    /// <param name="startIndex">The index within <paramref name="value"> at which to begin overwriting characters.
+    /// Overwriting is also allowed when <paramref name="startIndex"> equals the length of <paramref name="value">.</param>
+    /// <param name="newValue">The string to be used for overwriting characters. Overwriting will continue
+    /// past the length of <paramref name="value"> when applicable.</param>
+    /// <returns>The new string resulting from the overwrite.</returns>
+    /// <example>
+    /// <code>
+    /// Console.WriteLine("abc".Overwrite(0, "z"));
+    /// Console.WriteLine("abc".Overwrite(1, "z"));
+    /// Console.WriteLine("abc".Overwrite(1, "zy"));
+    /// Console.WriteLine("abc".Overwrite(2, "zy"));
+    /// Console.WriteLine("abc".Overwrite(3, "zy"));
+    /// </code>
+    /// Console Output:<br/>
+    /// zbc<br/>
+    /// azc<br/>
+    /// azy<br/>
+    /// abzy<br/>
+    /// abczy<br/>
+    /// </example>
+    public static string Overwrite(this string value, int startIndex, string newValue)
+    {
+        if (value == null)
+            throw new ArgumentNullException(nameof(value));
+
+        if (startIndex < 0)
+            throw new ArgumentOutOfRangeException(nameof(startIndex), startIndex, "startIndex must be >= 0.");
+
+        if (startIndex > value.Length)
+            throw new ArgumentOutOfRangeException(nameof(startIndex), "startIndex must be <= value.Length.");
+
+        if (newValue == null)
+            throw new ArgumentNullException(nameof(newValue));
+
+        return startIndex == value.Length
+            ? value + newValue
+            : value
+                .Remove(startIndex, Math.Min(value.Length - startIndex, newValue.Length))
+                .Insert(startIndex, newValue);
+    }
+}
