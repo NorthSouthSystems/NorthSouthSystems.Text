526c124caabdf13626a437e3055f207a469ce149
FreeAndWithBeer <30539567+FreeAndWithBeer@users.noreply.github.com>
2023-08-01 21:26:13 -0400

    Added IsNotNullAndNotX StringExtensions.

---
+++ FOSStrich.Text.Test/String/Primitive/Extensions/IsNotNullAndNotX.cs
@@ -0,0 +1,26 @@
+﻿namespace FOSStrich.Text;
+
+public static partial class StringExtensionsTests
+{
+    [TestClass]
+    public class IsNotNullAndNotX
+    {
+        [TestMethod]
+        public void IsNotNullAndNotEmpty()
+        {
+            Assert.IsFalse(StringExtensions.IsNotNullAndNotEmpty(null));
+            Assert.IsFalse(StringExtensions.IsNotNullAndNotEmpty(string.Empty));
+            Assert.IsTrue(StringExtensions.IsNotNullAndNotEmpty("a"));
+        }
+
+        [TestMethod]
+        public void IsNotNullAndNotWhiteSpace()
+        {
+            Assert.IsFalse(StringExtensions.IsNotNullAndNotWhiteSpace(null));
+            Assert.IsFalse(StringExtensions.IsNotNullAndNotWhiteSpace(string.Empty));
+            Assert.IsFalse(StringExtensions.IsNotNullAndNotWhiteSpace(" "));
+            Assert.IsFalse(StringExtensions.IsNotNullAndNotWhiteSpace(Environment.NewLine));
+            Assert.IsTrue(StringExtensions.IsNotNullAndNotWhiteSpace("a"));
+        }
+    }
+}
