131afc3673d2d5b1ff1529d3befa40b972c63df7
FreeAndWithBeer <30539567+FreeAndWithBeer@users.noreply.github.com>
2023-08-11 12:34:44 -0400

    Added WithColumnHeadersExtensions.

---
+++ FOSStrich.Text.Test/String/Columnar/WithColumnHeaders.cs
@@ -0,0 +1,63 @@
+﻿namespace FOSStrich.Text;
+
+public class WithColumnHeadersExtensionsTests
+{
+    [Fact]
+    public void Basic()
+    {
+        List<StringRowWrapper> result;
+        string nr = StringQuotedSignals.Csv.NewRow;
+
+        result = Result($"A{nr}1", new[] { "A" }, false);
+        result.Count.Should().Be(1);
+        result[0]["A"].Value.Should().Be("1");
+
+        result = Result($"A{nr}1", new[] { "A" }, true);
+        result.Count.Should().Be(1);
+        result[0]["A"].Value.Should().Be("1");
+
+        result = Result($"A,B{nr}1,2", new[] { "B", "A" }, false);
+        result.Count.Should().Be(1);
+        result[0]["A"].Value.Should().Be("1");
+        result[0]["B"].Value.Should().Be("2");
+
+        result = Result($"A,B{nr}1,2", new[] { "A", "B" }, true);
+        result.Count.Should().Be(1);
+        result[0]["A"].Value.Should().Be("1");
+        result[0]["B"].Value.Should().Be("2");
+
+        result = Result($"A,B,C{nr}1,2,3{nr}4,5,6", new[] { "A", "B", "C" }, true);
+        result.Count.Should().Be(2);
+        result[0]["A"].Value.Should().Be("1");
+        result[0]["B"].Value.Should().Be("2");
+        result[0]["C"].Value.Should().Be("3");
+        result[1]["A"].Value.Should().Be("4");
+        result[1]["B"].Value.Should().Be("5");
+        result[1]["C"].Value.Should().Be("6");
+    }
+
+    [Fact]
+    public void Exceptions()
+    {
+        Action act;
+        string nr = StringQuotedSignals.Csv.NewRow;
+
+        act = () => ((string[][])null).WithColumnHeaders(new[] { "A" }).ToList();
+        act.Should().ThrowExactly<ArgumentNullException>();
+
+        act = () => Result($"A{nr}1", new[] { "A", "B" }, false);
+        act.Should().ThrowExactly<ArgumentException>().WithMessage("Expected: B");
+
+        act = () => Result($"A,B{nr}1,2", new[] { "A" }, false);
+        act.Should().ThrowExactly<ArgumentException>().WithMessage("Unexpected: B");
+
+        act = () => Result($"A,B{nr}1,2", new[] { "B", "A" }, true);
+        act.Should().ThrowExactly<ArgumentException>().WithMessage("*expected order*");
+    }
+
+    private static List<StringRowWrapper> Result(string csv,
+            IEnumerable<string> expectedColumnNames, bool enforceExpectedColumnNamesOrder) =>
+        csv.SplitQuotedRows(StringQuotedSignals.Csv)
+            .WithColumnHeaders(expectedColumnNames, enforceExpectedColumnNamesOrder)
+            .ToList();
+}
---
+++ FOSStrich.Text/String/Columnar/WithColumnHeaders.cs
@@ -0,0 +1,70 @@
+﻿namespace FOSStrich.Text;
+
+public static class WithColumnHeadersExtensions
+{
+    /// <summary>
+    /// Provides an extension method for converting rows of fields (IEnumerable<string[]>) to
+    /// StringRowWrappers (IEnumerable<StringRowWrapper) by using the first row to construct
+    /// a StringRowWrapperFactory used to wrap subsequent rows. This is intended to be used fluently
+    /// after a call to any of the Split*Rows extensions methods.
+    /// </summary>
+    /// <param name="rowsOfFields">The rows of fields resulting from a Split*Rows invocation.</param>
+    /// <param name="expectedColumnNames">An optional parameter indicating that the caller would like
+    /// an ArgumentException thrown if the first row is not this exact set of fields.
+    /// (default = null)</param>
+    /// <param name="enforceExpectedColumnNamesOrder">An optional parameter indicating that the caller
+    /// would like an ArgumentException thrown if the first row is not the exact sequence of fields
+    /// specified by expectedColumnNames. (default = false)</param>
+    /// <returns>The rows of fields converted to StringRowWrappers excluding the first row representing
+    /// column headers.</returns>
+    public static IEnumerable<StringRowWrapper> WithColumnHeaders(this IEnumerable<string[]> rowsOfFields,
+        IEnumerable<string> expectedColumnNames = null, bool enforceExpectedColumnNamesOrder = false)
+    {
+        if (rowsOfFields == null)
+            throw new ArgumentNullException(nameof(rowsOfFields));
+
+        StringRowWrapperFactory rowWrapperFactory = null;
+        int rowIndex = 0;
+
+        foreach (string[] rowFields in rowsOfFields)
+        {
+            if (rowWrapperFactory == null)
+            {
+                if (expectedColumnNames?.Any() ?? false)
+                    ThrowIfIncorrectColumnNames();
+
+                void ThrowIfIncorrectColumnNames()
+                {
+                    var errors = new List<string>();
+
+                    errors.AddRange(expectedColumnNames.Except(rowFields).Select(c => "Expected: " + c));
+                    errors.AddRange(rowFields.Except(expectedColumnNames).Select(c => "Unexpected: " + c));
+
+                    if (!errors.Any() && enforceExpectedColumnNamesOrder && !rowFields.SequenceEqual(expectedColumnNames))
+                        errors.Add("Column names are not in the expected order.");
+
+                    if (errors.Any())
+                        throw new ArgumentException(string.Join(Environment.NewLine, errors));
+                }
+
+                rowWrapperFactory = new StringRowWrapperFactory(rowFields);
+            }
+            else
+            {
+                StringRowWrapper rowWrapper;
+
+                try { rowWrapper = rowWrapperFactory.Wrap(rowFields); }
+                catch (Exception exception)
+                {
+                    throw new ArgumentException(
+                        FormattableString.Invariant($"Error wrapping fields for row with 0-based index {rowIndex}."),
+                        exception);
+                }
+
+                yield return rowWrapper;
+            }
+
+            rowIndex++;
+        }
+    }
+}
