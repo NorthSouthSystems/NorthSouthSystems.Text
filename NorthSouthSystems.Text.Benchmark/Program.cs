0d9ad44c5c6192eed6f73dd36b04348ddc9366c0
FreeAndWithBeer <30539567+FreeAndWithBeer@users.noreply.github.com>
2024-02-03 14:06:37 -0500

    Created Benchmark project.

---
+++ NorthSouthSystems.Text.Benchmark/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace NorthSouthSystems.Text;
+
+using BenchmarkDotNet.Running;
+
+internal class Program
+{
+    internal const string CatalogDataGovDirectory = @"C:\CatalogDataGov";
+
+    private static void Main(string[] args) =>
+        BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+}
---
+++ NorthSouthSystems.Text.Benchmark/SplitQuotedRowsBenchmarks.cs
@@ -0,0 +1,36 @@
+﻿namespace NorthSouthSystems.Text;
+
+using BenchmarkDotNet.Engines;
+using System.IO;
+
+[MemoryDiagnoser]
+public class SplitQuotedRowsBenchmarks
+{
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        string realEstateSalesCsvFilepath = Path.Combine(Program.CatalogDataGovDirectory, "Real_Estate_Sales_2001-2020_GL.csv");
+
+        _csvLinuxNewLines = File.ReadAllText(realEstateSalesCsvFilepath);
+
+        if (_csvLinuxNewLines.Contains("\r\n"))
+            throw new FormatException();
+
+        _csvWindowsNewLines = _csvLinuxNewLines.Replace("\n", "\r\n");
+    }
+
+    private string _csvLinuxNewLines;
+    private string _csvWindowsNewLines;
+
+    private readonly Consumer _consumer = new();
+
+    [Benchmark]
+    public void LinuxNewLines() => _csvLinuxNewLines
+        .SplitQuotedRows(new(",", "\"", "\n", string.Empty))
+        .Consume(_consumer);
+
+    [Benchmark]
+    public void WindowsNewLines() => _csvWindowsNewLines
+        .SplitQuotedRows(new(",", "\"", "\r\n", string.Empty))
+        .Consume(_consumer);
+}
