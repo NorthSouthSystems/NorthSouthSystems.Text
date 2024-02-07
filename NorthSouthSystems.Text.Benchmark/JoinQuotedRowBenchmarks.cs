1d62f9d4c36f19cb4ded771064d5152faee240fd
FreeAndWithBeer <30539567+FreeAndWithBeer@users.noreply.github.com>
2024-02-06 21:51:52 -0500

    Added JoinQuotedRowBenchmarks.

---
+++ NorthSouthSystems.Text.Benchmark/JoinQuotedRowBenchmarks.cs
@@ -0,0 +1,38 @@
+﻿namespace NorthSouthSystems.Text;
+
+using System.IO;
+
+[MemoryDiagnoser]
+public class JoinQuotedRowBenchmarks
+{
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        string realEstateSalesCsvFilepath = Path.Combine(Program.CatalogDataGovDirectory, "Real_Estate_Sales_2001-2020_GL.csv");
+
+        string csvLinuxNewLines = File.ReadAllText(realEstateSalesCsvFilepath);
+
+        if (csvLinuxNewLines.Contains("\r\n"))
+            throw new FormatException();
+
+        _rowsFields = csvLinuxNewLines
+            .SplitQuotedRows(new(",", "\"", "\n", string.Empty))
+            .ToArray();
+    }
+
+    private string[][] _rowsFields;
+
+    [Benchmark]
+    public void LinuxNewLines()
+    {
+        foreach (var fields in _rowsFields)
+            fields.JoinQuotedRow(new(",", "\"", "\n", string.Empty));
+    }
+
+    [Benchmark]
+    public void WindowsNewLines()
+    {
+        foreach (var fields in _rowsFields)
+            fields.JoinQuotedRow(new(",", "\"", "\r\n", string.Empty));
+    }
+}
