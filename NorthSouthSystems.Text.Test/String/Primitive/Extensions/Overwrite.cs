d0a4bb31ae93557aff3968354a06c5f478a9d50f
FreeAndWithBeer <30539567+FreeAndWithBeer@users.noreply.github.com>
2024-02-18 14:38:25 -0500

    Added more Overwrite tests (no particular need - was experiementing with perf improvements that ultimately weren't worth the added complexity).

--- NorthSouthSystems.Text.Test/String/Primitive/Extensions/Overwrite.cs
+++ NorthSouthSystems.Text.Test/String/Primitive/Extensions/Overwrite.cs
@@ -5,11 +5,35 @@ public class StringExtensionsTests_Overwrite
     [Fact]
     public void Basic()
     {
+        "a".Overwrite(0, "z").Should().Be("z");
+        "a".Overwrite(1, "z").Should().Be("az");
+
+        "ab".Overwrite(0, "z").Should().Be("zb");
+        "ab".Overwrite(1, "z").Should().Be("az");
+        "ab".Overwrite(2, "z").Should().Be("abz");
+
         "abc".Overwrite(0, "z").Should().Be("zbc");
         "abc".Overwrite(1, "z").Should().Be("azc");
+        "abc".Overwrite(2, "z").Should().Be("abz");
+        "abc".Overwrite(3, "z").Should().Be("abcz");
+
+        "a".Overwrite(0, "zy").Should().Be("zy");
+        "a".Overwrite(1, "zy").Should().Be("azy");
+
+        "ab".Overwrite(0, "zy").Should().Be("zy");
+        "ab".Overwrite(1, "zy").Should().Be("azy");
+        "ab".Overwrite(2, "zy").Should().Be("abzy");
+
+        "abc".Overwrite(0, "zy").Should().Be("zyc");
         "abc".Overwrite(1, "zy").Should().Be("azy");
         "abc".Overwrite(2, "zy").Should().Be("abzy");
         "abc".Overwrite(3, "zy").Should().Be("abczy");
+
+        "abcd".Overwrite(0, "zy").Should().Be("zycd");
+        "abcd".Overwrite(1, "zy").Should().Be("azyd");
+        "abcd".Overwrite(2, "zy").Should().Be("abzy");
+        "abcd".Overwrite(3, "zy").Should().Be("abczy");
+        "abcd".Overwrite(4, "zy").Should().Be("abcdzy");
     }
 
     [Fact]
