dff4c10506ed93498f7a38b8d18aa57082f8d4a7
FreeAndWithBeer <30539567+FreeAndWithBeer@users.noreply.github.com>
2024-07-22 18:49:30 -0400

    Moved code to create StringQuotedSignalsFound file.

--- NorthSouthSystems.Text/String/Columnar/Quoted/StringQuotedSignals.cs
+++ NorthSouthSystems.Text/String/Columnar/Quoted/StringQuotedSignals.cs
@@ -79,23 +79,3 @@ public sealed class StringQuotedSignals
     internal string EscapedQuote { get; }
     internal string EscapedEscape { get; }
 }
-
-internal readonly struct StringQuotedSignalsFound
-{
-    internal StringQuotedSignalsFound(StringQuotedSignals signals, string field)
-    {
-        DelimiterFound = signals.Delimiters.Any(field.Contains);
-        NewRowFound = signals.NewRows.Any(field.Contains);
-        QuoteFound = signals.QuoteIsSpecified && field.Contains(signals.Quote);
-        EscapeFound = signals.EscapeIsSpecified && field.Contains(signals.Escape);
-
-        RequiresQuotingOrEscaping = DelimiterFound || QuoteFound || NewRowFound;
-    }
-
-    internal readonly bool DelimiterFound { get; }
-    internal readonly bool NewRowFound { get; }
-    internal readonly bool QuoteFound { get; }
-    internal readonly bool EscapeFound { get; }
-
-    internal readonly bool RequiresQuotingOrEscaping { get; }
-}
---
+++ NorthSouthSystems.Text/String/Columnar/Quoted/StringQuotedSignalsFound.cs
@@ -0,0 +1,21 @@
+﻿namespace NorthSouthSystems.Text;
+
+internal readonly struct StringQuotedSignalsFound
+{
+    internal StringQuotedSignalsFound(StringQuotedSignals signals, string field)
+    {
+        DelimiterFound = signals.Delimiters.Any(field.Contains);
+        NewRowFound = signals.NewRows.Any(field.Contains);
+        QuoteFound = signals.QuoteIsSpecified && field.Contains(signals.Quote);
+        EscapeFound = signals.EscapeIsSpecified && field.Contains(signals.Escape);
+
+        RequiresQuotingOrEscaping = DelimiterFound || QuoteFound || NewRowFound;
+    }
+
+    internal readonly bool DelimiterFound { get; }
+    internal readonly bool NewRowFound { get; }
+    internal readonly bool QuoteFound { get; }
+    internal readonly bool EscapeFound { get; }
+
+    internal readonly bool RequiresQuotingOrEscaping { get; }
+}
